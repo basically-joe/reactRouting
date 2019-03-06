a1be8dbf022b84531083ed5b17e93260fe74ecdb
Joe McColl <joe4kool@gmail.com>
2019-03-06 10:42:25 +0000

    installed npm react-router-dom

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -4,6 +4,9 @@ const Contact = () => (
   <div>
     <h4>Contact</h4>
     <p>Some information about contacting us.</p>
+    <ul>
+    <li>Email: randomemail@email.com</li>
+    </ul>
   </div>
 );
 
