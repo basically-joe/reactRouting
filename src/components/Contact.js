3ca386570aa7b1e9538cd88df386cbcdd4d30949
Joe McColl <joe4kool@gmail.com>
2019-03-06 11:00:50 +0000

    Added contact link, working in browser

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -3,7 +3,7 @@ import React from "react";
 const Contact = () => (
   <div>
     <h4>Contact</h4>
-    <p>Some information about contacting us.</p>
+    <p>Some information about contacting us:</p>
     <ul>
     <li>Email: randomemail@email.com</li>
     <li>Tel: 01390325789</li>
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -12,6 +12,9 @@ const Navbar = () => (
     <li>
       <Link to="/pricing">Pricing</Link>
     </li>
+    <li>
+      <Link to="/contact">Contact</Link>
+    </li>
   </ul>
 );
 
