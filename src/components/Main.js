1b457285e3c8ca489fa10e5b39d00bc3dc80053a
Joe McColl <joe4kool@gmail.com>
2019-03-06 10:59:29 +0000

    NavBar code added, showing in browser

--- src/components/Main.js
+++ src/components/Main.js
@@ -4,19 +4,19 @@ import About from "./About";
 import Home from "./Home";
 import Pricing from "./Pricing";
 import Contact from "./Contact";
+import NavBar from "./NavBar";
 
 class Main extends Component {
   constructor() {
     super();
     this.state = { page: null };
-
-
   }
 
   render() {
     return (
       <Router>
           <Fragment>
+            <NavBar />
             <Route exact path="/" component={Home}/>
             <Route path="/about" component={About}/>
             <Route path="/pricing" component={Pricing}/>
